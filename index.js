ca034271e3f0e1569c1258c9cd101c9fb6c30de1
Sumit <sumit@kodnest.com>
2022-12-27 13:37:34 +0530

    optional commit

--- index.js
+++ index.js
@@ -3,11 +3,22 @@ const connect_db = require("./utils/db_connect");
 const app = express();
 const authRouter = require("./router/auth");
 const PORT = process.env.PORT || 5001;
+const ejs = require("ejs");
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.set("view engine", "ejs");
+
+app.use(express.static("public"));
+app.get("/", async (req, res) => {
+  res.render("index", { title: "Authentication" });
 });
 
+app.get("/profile", (req, res) => {
+  res.render("profile", { title: "Profile" });
+});
+
+app.get("/logout", (req, res) => {
+  res.render("logout", { title: "Logout" });
+});
 app.use("/auth", authRouter);
 
 app.listen(PORT, async () => {
